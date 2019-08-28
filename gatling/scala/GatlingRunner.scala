d494d51a07d1db1d0d0d201b1c9076514421e055
Das, Tarun <tarun.das@c.wizards.com>
2019-08-28 16:35:42 -0700

    CQ-1072 Weekly 5 TC's For Event Reservation Filter Event Names and Assertions

---
+++ gatling/scala/GatlingRunner.scala
@@ -0,0 +1,20 @@
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+import simulations._
+import finalSimulation._
+
+
+object GatlingRunner {
+
+  def main(args: Array[String]): Unit = {
+
+    // this is where you specify the class you want to run
+    val simClass = classOf[EventReservationTC].getName
+
+    val props = new GatlingPropertiesBuilder
+    props.simulationClass(simClass)
+
+    Gatling.fromMap(props.build)
+  }
+
+}
